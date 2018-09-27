072a93014753e55710741eb45997b62e77d1e3ad
borgesan <borgesan@ifi.uio.no>
2018-09-27 15:33:48 +0200

    get_dataset/model loading from project directories

--- data_utils/datasets.py
+++ data_utils/datasets.py
@@ -1,6 +1,5 @@
 # Standard library imports:
-from torch.utils.data import Dataset
-from torchvision.datasets import MNIST
+from importlib import import_module
 # Local imports:
 from data_utils.transforms import get_composed_transforms
 
@@ -11,12 +10,20 @@ def get_dataset(ds_config):
     :param ds_config: 'dataset' section of configuration file
     :return: instance of dataset object
     """
-    dataset_type = ds_config['type']
+    # Separate module and class names:
+    mod_name, class_name = ds_config['type'].rsplit('.', 1)
 
+    # Get module specified in configuration file:
     try:
-        dataset = eval(dataset_type)
-    except NameError as e:
-        raise NameError(f"Data loader ({dataset_type}) not found.")
+        mod = import_module(mod_name)
+    except ImportError:
+        raise ImportError(f"No module named {mod_name} found.")
+
+    # Get class specified in configuration file:
+    try:
+        dataset = getattr(mod, class_name)
+    except AttributeError:
+        raise AttributeError(f"No function '{class_name}' found in module '{mod_name}'")
 
     composed_transforms = get_composed_transforms(ds_config['transforms'])
 
@@ -24,37 +31,3 @@ def get_dataset(ds_config):
                    **ds_config['kwargs'])
 
 
-def MnistDataset(*args, **kwargs):
-    """Function returning MNIST class (as it's already implemented)."""
-    return MNIST(*args, **kwargs)
-
-
-class CustomDataset(Dataset):
-    """Template for implementing your own custom dataset."""
-    def __init__(self, root, train=True, transform=None):
-        self.root = root
-        self.transform = transform
-        self.train = train
-
-        # Implement the rest of your initialization code.
-
-        if self.train:
-            self.train_data = None # Implement your data loading
-            self.train_labels = None # Implement your data loading
-        else:
-            self.test_data = None # Implement your data loading
-            self.test_labels = None # Implement your data loading
-
-    def __len__(self):
-        if self.train:
-            return len(self.train_data)
-        else:
-            return len(self.test_data)
-
-    def __getitem__(self, index):
-        if self.train:
-            sample, target = self.train_data[index], self.train_labels[index]
-        else:
-            sample, target = self.test_data[index], self.test_labels[index]
-
-        return sample, target
